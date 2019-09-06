6f384865b687bb8db971b1de4bbf2cf14bf3b547
Felipunky <feli20gutierrez@hotmail.com>
2019-09-05 21:24:52 -0500

    Prettify
    Make it pretty now!

--- InstagramFollowers/Form1.Designer.cs
+++ InstagramFollowers/Form1.Designer.cs
@@ -45,19 +45,20 @@
             // 
             this.UsernameText.Location = new System.Drawing.Point(98, 14);
             this.UsernameText.Name = "UsernameText";
-            this.UsernameText.Size = new System.Drawing.Size(609, 20);
+            this.UsernameText.Size = new System.Drawing.Size(609, 21);
             this.UsernameText.TabIndex = 0;
             this.UsernameText.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // RunButton
             // 
+            this.RunButton.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.RunButton.Location = new System.Drawing.Point(648, 359);
             this.RunButton.Margin = new System.Windows.Forms.Padding(2);
             this.RunButton.Name = "RunButton";
             this.RunButton.Size = new System.Drawing.Size(144, 83);
             this.RunButton.TabIndex = 5;
             this.RunButton.Text = "Run";
-            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.UseVisualStyleBackColor = false;
             this.RunButton.Click += new System.EventHandler(this.Button1_Click);
             // 
             // PasswordText
@@ -65,7 +66,7 @@
             this.PasswordText.Location = new System.Drawing.Point(98, 59);
             this.PasswordText.Name = "PasswordText";
             this.PasswordText.PasswordChar = '*';
-            this.PasswordText.Size = new System.Drawing.Size(609, 20);
+            this.PasswordText.Size = new System.Drawing.Size(609, 21);
             this.PasswordText.TabIndex = 1;
             this.PasswordText.TextChanged += new System.EventHandler(this.Password_TextChanged);
             // 
@@ -74,7 +75,7 @@
             this.UsernameLabel.AutoSize = true;
             this.UsernameLabel.Location = new System.Drawing.Point(37, 17);
             this.UsernameLabel.Name = "UsernameLabel";
-            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.Size = new System.Drawing.Size(58, 13);
             this.UsernameLabel.TabIndex = 3;
             this.UsernameLabel.Text = "Username";
             this.UsernameLabel.Click += new System.EventHandler(this.Username_Click);
@@ -84,7 +85,7 @@
             this.PasswordLabel.AutoSize = true;
             this.PasswordLabel.Location = new System.Drawing.Point(37, 62);
             this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.Size = new System.Drawing.Size(55, 13);
             this.PasswordLabel.TabIndex = 4;
             this.PasswordLabel.Text = "Password";
             // 
@@ -93,15 +94,16 @@
             this.WaitTime.AutoSize = true;
             this.WaitTime.Location = new System.Drawing.Point(37, 154);
             this.WaitTime.Name = "WaitTime";
-            this.WaitTime.Size = new System.Drawing.Size(105, 13);
+            this.WaitTime.Size = new System.Drawing.Size(108, 13);
             this.WaitTime.TabIndex = 6;
             this.WaitTime.Text = "Wait time in seconds";
             // 
             // WaitTimeText
             // 
+            this.WaitTimeText.BackColor = System.Drawing.SystemColors.Window;
             this.WaitTimeText.Location = new System.Drawing.Point(146, 151);
             this.WaitTimeText.Name = "WaitTimeText";
-            this.WaitTimeText.Size = new System.Drawing.Size(559, 20);
+            this.WaitTimeText.Size = new System.Drawing.Size(559, 21);
             this.WaitTimeText.TabIndex = 3;
             // 
             // FriendLabel
@@ -109,7 +111,7 @@
             this.FriendLabel.AutoSize = true;
             this.FriendLabel.Location = new System.Drawing.Point(37, 110);
             this.FriendLabel.Name = "FriendLabel";
-            this.FriendLabel.Size = new System.Drawing.Size(36, 13);
+            this.FriendLabel.Size = new System.Drawing.Size(38, 13);
             this.FriendLabel.TabIndex = 8;
             this.FriendLabel.Text = "Friend";
             // 
@@ -117,14 +119,14 @@
             // 
             this.FriendText.Location = new System.Drawing.Point(98, 107);
             this.FriendText.Name = "FriendText";
-            this.FriendText.Size = new System.Drawing.Size(609, 20);
+            this.FriendText.Size = new System.Drawing.Size(609, 21);
             this.FriendText.TabIndex = 2;
             this.FriendText.TextChanged += new System.EventHandler(this.TextBox1_TextChanged_1);
             // 
             // FollowersOrFollowingCheckBox
             // 
             this.FollowersOrFollowingCheckBox.AutoSize = true;
-            this.FollowersOrFollowingCheckBox.Location = new System.Drawing.Point(385, 198);
+            this.FollowersOrFollowingCheckBox.Location = new System.Drawing.Point(407, 198);
             this.FollowersOrFollowingCheckBox.Name = "FollowersOrFollowingCheckBox";
             this.FollowersOrFollowingCheckBox.Size = new System.Drawing.Size(15, 14);
             this.FollowersOrFollowingCheckBox.TabIndex = 4;
@@ -136,7 +138,7 @@
             this.FollowOrFollowingLabel.AutoSize = true;
             this.FollowOrFollowingLabel.Location = new System.Drawing.Point(37, 198);
             this.FollowOrFollowingLabel.Name = "FollowOrFollowingLabel";
-            this.FollowOrFollowingLabel.Size = new System.Drawing.Size(342, 13);
+            this.FollowOrFollowingLabel.Size = new System.Drawing.Size(364, 13);
             this.FollowOrFollowingLabel.TabIndex = 9;
             this.FollowOrFollowingLabel.Text = "Followers or Following? Checked for followers, Unchecked for following";
             // 
@@ -144,6 +146,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.FollowersOrFollowingCheckBox);
             this.Controls.Add(this.FollowOrFollowingLabel);
@@ -156,8 +159,10 @@
             this.Controls.Add(this.PasswordText);
             this.Controls.Add(this.RunButton);
             this.Controls.Add(this.UsernameText);
+            this.Font = new System.Drawing.Font("Interstate Regular", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "InstagramFollowers";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
